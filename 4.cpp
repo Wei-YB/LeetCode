fe221e34eed1c13376476423497e0ce9c13fff5a
Owen Wei lab PC <weiyibo97@gmail.com>
2021-03-26 15:38:58 +0800

    start this project

---
+++ 4.cpp
@@ -0,0 +1,19 @@
+#include "util.h"
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+    }
+
+    pair<int, int> findKth(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+
+
+
+
+int main() {
+    Solution s;
+     cout << s.findMedianSortedArrays() << endl;
+}
---
+++ util.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <memory>
+
+
+
+
+using namespace std;
